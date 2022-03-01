69d528fd461c4d22af200908726d9f8f2ac2e4ad
maharani14 <99779863+maharani14@users.noreply.github.com>
2022-03-01 23:24:57 +0800

    day11_java_int_to_string.java

---
+++ day11_java_int_to_string.java
@@ -0,0 +1,10 @@
+//Write your code here
+    //String s = ""+n;
+    //String s = String.valueOf(n);
+    //String s = Integer.toString(n);
+    //String s = String.format("%d", n);
+    
+    Integer i = new Integer (n);
+    StringBuilder sb = new StringBuilder(); 
+    sb.append(i);
+    String s = sb.toString();
